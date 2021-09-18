8b8087ba76534257053c1212353784f3d0916bd2
Kengo Higashi <shigure@higashi1999.onmicrosoft.com>
2021-09-19 03:54:53 +0900

    create interface schematic

---
+++ src/lib/interface/interface.factory.ts
@@ -0,0 +1,55 @@
+import { join, Path, strings } from '@angular-devkit/core';
+import {
+    apply,
+    branchAndMerge,
+    chain,
+    mergeWith,
+    move,
+    Rule,
+    SchematicContext,
+    SchematicsException,
+    template,
+    Tree,
+    url,
+} from '@angular-devkit/schematics';
+import { isNullOrUndefined } from 'util';
+import { Location, NameParser } from '../../utils/name.parser';
+import { mergeSourceRoot } from '../../utils/source-root.helpers';
+import { ServiceOptions } from './intereface.schema';
+
+export function main(options: ServiceOptions): Rule {
+    options = transform(options);
+    return (tree: Tree, context: SchematicContext) => {
+        return branchAndMerge(
+            chain([
+                mergeSourceRoot(options),
+                mergeWith(generate(options)),
+            ]),
+        )(tree, context);
+    };
+}
+
+function transform(source: ServiceOptions): ServiceOptions {
+    const target: ServiceOptions = Object.assign({}, source);
+
+    if (isNullOrUndefined(target.name)) {
+        throw new SchematicsException('Option (name) is required.');
+    }
+    const location: Location = new NameParser().parse(target);
+    target.name = strings.dasherize(location.name);
+    target.path = strings.dasherize(location.path);
+    target.language = 'ts';
+
+    return target;
+}
+
+function generate(options: ServiceOptions) {
+    return (context: SchematicContext) =>
+        apply(url(join('./files' as Path, options.language)), [
+            template({
+                ...strings,
+                ...options,
+            }),
+            move(options.path),
+        ])(context);
+}
